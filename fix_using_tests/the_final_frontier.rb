74a6429cfbc9b6365f22a4fa598982dca204b231
Deydra Cavazos <deydra@cavazospartners.com>
2021-02-06 01:47:57 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
@@ -10,7 +8,7 @@ def state_log(star_date)
 end
 
 def crew
-  ["Hello, Geordi.", "Hello, Data.", "Hello, Worf.", "Hello, William.", "Hello, Beverly.", "Hello, Deanna."]
+  ["Geordi", "Data", "Worf", "William", "Beverly", "Deanna"]
 end
 
 def greet_crew (crew)
@@ -18,6 +16,7 @@ def greet_crew (crew)
 end
 
 def engage
-  state_log(star_date)
+  date = generate_star_date
+  state_log(date)
   greet_crew(crew)
 end
